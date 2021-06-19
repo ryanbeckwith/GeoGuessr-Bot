f1d2b9abe1ee13ed6ad854efd5de9f441213a3b9
Sohom Sen <sohom416@hotmail.com>
2021-06-19 13:31:24 -0400

    Created Discord implementation
    This file is for running the original script with the Discord api.

---
+++ geoguessrdiscordbot.py
@@ -0,0 +1,39 @@
+
+#imports
+from geoguessr import *
+from dev import *
+import discord
+from discord.ext import commands
+import time
+
+
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    print('Discord Bot Initiated')
+""" 
+@client.event
+async def on_command_error(ctx,error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Invalid command! Please check !help for all available GeoGuessr bot commands!")
+    if isinstance(error, commands.CommandInvokeError):
+        await ctx.send("Invalid command! Remember to type the map and rule! Please check !help for all available GeoGuessr bot commands!") """
+
+@client.command()
+async def geo(ctx, *args):
+    user_map = args[0]
+    rule = args[1]
+
+    if user_map in maps and rule in options:
+        await ctx.send("Game found! " + user_map + " " + rule)
+        game_link = game.map_generator(user_map, rule)
+        await ctx.send("Enjoy the game! " + game_link)          
+    else:
+        await ctx.send("Uh oh! Game link could not be generated based on your input. Please try again! Reference !help for help!")
+
+
+game = GeoGuessorBot()
+print("Game Installed")
+game.login()
+client.run(token)
