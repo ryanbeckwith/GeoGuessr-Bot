1a6c05414a6817296dc665e7e07c22e9d9a82ca1
Sohom Sen <sohom416@hotmail.com>
2021-06-22 17:19:31 -0400

    Added more comments

--- geoguessr.py
+++ geoguessr.py
@@ -2,10 +2,10 @@
 GeoGuessr Bot
 Author: Sohom Sen
 Email: sohom416@hotmail.com
+Current Version: 0.2-alpha
 """
 
 #imports
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -18,23 +18,24 @@ import time, sys
 import os
 from os import environ
 
+# Environment varible declarations
 GOOGLE_CHROME_PATH = environ['GOOGLE_CHROME_PATH']
 CHROMEDRIVER_PATH = environ['CHROMEDRIVER_PATH']
 USERNAME = environ['USERNAME']
 PASSWORD = environ['PASSWORD']
 
 class GeoGuessorBot():
-    # Initializes Chrome driver and browser functions
     def __init__(self):
+        # Initializes Chrome driver and browser functions
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         self.driver = webdriver.Chrome(CHROMEDRIVER_PATH, options = chrome_options)
         self.wait = WebDriverWait(self.driver,20)
         print("Bot Initialized")
-
-    # Function for logging in GeoGuessrPro account.
+    
     def login(self):
+        # Function for logging in GeoGuessrPro account.
         self.driver.get("https://www.geoguessr.com/")
         loginButton = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[normalize-space()='Log in']")))
         loginButton.click()
@@ -49,6 +50,7 @@ class GeoGuessorBot():
         time.sleep(1)
     
     def default(self):
+        # This function is called when the game setting is set to default by the user. 
         invite = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@type='button']")))
         invite.click()
         start = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@class='button button--medium button--primary margin--top']")))
@@ -56,6 +58,7 @@ class GeoGuessorBot():
         time.sleep(5)
 
     def no_move(self):
+        # This function is called when the game setting is set to no move by the user.
         nmSelect = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//label[normalize-space()='No move']")))
         nmSelect.click()
         invite = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@type='button']")))
@@ -65,6 +68,7 @@ class GeoGuessorBot():
         time.sleep(5)    
     
     def no_zoom(self):
+        # This function is called when the game setting is set to no zoom by the user.
         nzSelect = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//label[normalize-space()='No zoom']")))
         nzSelect.click()
         invite = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@type='button']")))
@@ -74,6 +78,7 @@ class GeoGuessorBot():
         time.sleep(5)   
 
     def no_move_zoom(self):
+        # This function is called when the game setting is set to no move, no zoom by the user.
         nzSelect = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//label[normalize-space()='No move, no zoom']")))
         nzSelect.click()
         invite = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@type='button']")))
@@ -83,6 +88,7 @@ class GeoGuessorBot():
         time.sleep(5)
     
     def no_move_zoom_pan(self):
+        # This function is called when the game setting is set to no move, no pan, no zoom by the user.
         nzSelect = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//label[normalize-space()='No move, no pan, no zoom']")))
         nzSelect.click()   
         invite = self.wait.until(EC.element_to_be_clickable((By.XPATH,"//button[@type='button']")))
@@ -92,14 +98,16 @@ class GeoGuessorBot():
         time.sleep(5)
 
     def game_setting(self):
+        # Function for checking if the game rule menu is being displayed. Returns False if the element is not found.
         try:
             self.driver.find_element_by_xpath("//div[@class='game-settings__detailed-settings']")
         except NoSuchElementException:
             return False
         return True
 
-    # Function for generating GeoGuessr game links
+    
     def map_generator(self, map, option):
+        # Function for generating GeoGuessr game links
         map_checked = checkMap(map) # checks for a valid map
         if map_checked in maps:
             map_final = checkCustom(map) # checks for custom GeoGuessr Maps (unofficial ones have unique hash values instead of strings)
@@ -171,16 +179,16 @@ class GeoGuessorBot():
 
 
 def main():
-    browser = GeoGuessorBot()
+    browser = GeoGuessorBot() #initiates GeoGuessrBot
     browser.login()
     map,option = input("Enter the Map you want and rule: ").split() 
-    geoguessrlink = browser.map_generator(map,option)
-    if geoguessrlink == False:
+    geoguessrlink = browser.map_generator(map,option) # Generates link
+    if geoguessrlink == False: # Checks if the game link was properly generated. If not, then error is sent to the user.
         print("Error Occured. Either the map or rule is incorrect. Please run program again.")
         sys.exit()
     else:
-        print("Game link generated:")
-        print(geoguessrlink)
+        print("Game link generated:") 
+        print(geoguessrlink) #User receives the generated game link
         exitKey = input("Press X to close program: ")
         if exitKey == "x":
             sys.exit()
