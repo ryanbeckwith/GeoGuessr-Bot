5768f56a2d1104d256a743a312b0964a054dde64
Sohom Sen <sohom416@hotmail.com>
2021-06-21 20:38:48 -0400

    Added cooldown for geo command

--- geoguessrdiscordbot.py
+++ geoguessrdiscordbot.py
@@ -6,6 +6,7 @@ from dev import *
 import discord
 from discord.ext import commands
 import time
+from discord.ext.commands import cooldown, BucketType, CommandOnCooldown
 
 
 client = commands.Bot(command_prefix = '-')
@@ -13,15 +14,20 @@ client = commands.Bot(command_prefix = '-')
 @client.event
 async def on_ready():
     print('Discord Bot Initiated')
-""" 
+
 @client.event
 async def on_command_error(ctx,error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("Invalid command! Please check !help for all available GeoGuessr bot commands!")
     if isinstance(error, commands.CommandInvokeError):
         await ctx.send("Invalid command! Remember to type the map and rule! Please check !help for all available GeoGuessr bot commands!") """
+        ctx.command.reset_cooldown(ctx)
+    if isinstance(error, CommandOnCooldown):
+        await ctx.send(f"Slow down! Use the bot in {error.retry_after:,.2f} seconds!")
+        
 
 @client.command()
+@commands.cooldown(1,30, BucketType.user)
 async def geo(ctx, arg1, arg2):
     user_map = arg1
     rule = arg2
@@ -32,11 +38,13 @@ async def geo(ctx, arg1, arg2):
         await ctx.send("Enjoy the game! " + game_link)          
     else:
         await ctx.send("Uh oh! Game link could not be generated based on your input. Reference !help for help!")
+        ctx.command.reset_cooldown(ctx)
 
 @geo.error
 async def geo_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("You forgot to add a required option! Remember the format is !geo [map] [game rule]")
+        ctx.command.reset_cooldown(ctx)
 
 
 game = GeoGuessorBot()
