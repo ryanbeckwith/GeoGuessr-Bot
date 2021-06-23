0686b4af797ff6601959e116ba4153a8544f7f16
Sohom Sen <sohom416@hotmail.com>
2021-06-22 23:37:44 -0400

    temp
    temp

--- geoguessrdiscordbot.py
+++ geoguessrdiscordbot.py
@@ -178,7 +178,7 @@ async def credits(ctx):
     await ctx.send(embed = about)
 
 @client.command(aliases = ["g", "game"])
-@commands.cooldown(1,30, BucketType.server)
+@commands.cooldown(1,30, BucketType.guild)
 async def geo(ctx, arg1, arg2):
     # this command is the call for using the GeoGuessr.py functions. It takes in two arugments from the user in discord: map and rule
     user_map = arg1.lower()
